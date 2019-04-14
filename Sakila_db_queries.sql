92669ccf49a1bee17f4544d96357c2f6ea522cd0
OvaisPatoli <ovaispatoli@gmail.com>
2019-04-13 21:01:12 -0500

    Finished Section 2 Queries

--- Sakila_db_queries.sql
+++ Sakila_db_queries.sql
@@ -6,4 +6,14 @@ SELECT first_name, last_name FROM actor;
 -- Question (1b): Display First and Last name in a single column in upper case letters. Name column 'Actor Name'
 SELECT UPPER(CONCAT(first_name, " ", last_name)) AS 'Actor Name' FROM actor;
 
+-- Question (2a): Find the ID Number, first name, last name of an actor of whome you only have the fist name. 
+SELECT actor_id, first_name, last_name FROM actor WHERE first_name = 'Joe';
 
+-- Question (2b): Find all actors whose last name contain the letters 'GEN'
+SELECT actor_id, first_name, last_name FROM actor WHERE last_name LIKE "%GEN%";
+
+-- Question (2c): Find all actors whose last name contains the ltters 'LI'. Order rows by last name and first name
+SELECT last_name, first_name FROM actor WHERE last_name LIKE "%LI%" ORDER BY last_name, first_name;
+
+-- Question (2d): Using 'IN', display the 'country_id' and 'country' coluns of the countries: Afghanistan, Bangladesh, China
+SELECT country_id, country FROM country WHERE country in ("Afghanistan", "Bangladesh", "China");
